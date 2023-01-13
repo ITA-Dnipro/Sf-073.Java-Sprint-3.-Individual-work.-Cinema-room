947cb0eb7682c21cf94430da329650ff1d0f37eb
mishoto <mihail.dev.io@gmail.com>
2023-01-13 07:30:18 +0200

    create stage-two/ReservationService

---
+++ mihail/stage-two/src/main/java/stage/two/services/ReservationService.java
@@ -0,0 +1,16 @@
+package stage.two.services;
+
+import stage.one.models.Room;
+import stage.one.models.Seat;
+
+public class ReservationService {
+
+    private Room room;
+
+    public boolean isAvailable(Seat seat) {
+        //TODO implementation
+        return false;
+    }
+
+
+}
