c10fd6b744fa002ec1c8840640c0cc7f84f43ea3
mishoto <mihail.dev.io@gmail.com>
2023-01-16 12:38:40 +0200

    stage-3-4/update Constants for all routes

---
+++ mihail/stage-three-four/src/main/java/stage3/four/utils/Constants.java
@@ -0,0 +1,11 @@
+package stage3.four.utils;
+
+public class Constants {
+
+    private Constants() {}
+
+    public static final String POST_STATS_URL = "/stats";
+    public static final String GET_ROOM_SEATS_URL = "/seats";
+    public static final String POST_TICKET_PURCHASE_URL = "/purchase";
+    public static final String POST_TICKET_REFUND_URL = "/return";
+}
