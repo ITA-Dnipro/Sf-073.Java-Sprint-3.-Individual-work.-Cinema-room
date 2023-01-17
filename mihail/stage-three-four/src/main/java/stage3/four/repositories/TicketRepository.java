4468d20a099a9ef7cb4978d194ab070aad8bebb0
mishoto <mihail.dev.io@gmail.com>
2023-01-17 14:02:43 +0200

    stage-3-4/add some methods to TicketRepository

--- mihail/stage-three-four/src/main/java/stage3/four/repositories/TicketRepository.java
+++ mihail/stage-three-four/src/main/java/stage3/four/repositories/TicketRepository.java
@@ -1,11 +1,13 @@
 package stage3.four.repositories;
 
 import com.google.common.collect.HashBasedTable;
-import com.google.common.collect.Multiset;
 import com.google.common.collect.Table;
 import org.springframework.stereotype.Repository;
 import stage3.four.models.Seat;
 import stage3.four.models.Ticket;
+import stage3.four.modelsDTO.PurchasedTicketDTO;
+import stage3.four.modelsDTO.TicketDTO;
+import stage3.four.modelsDTO.TokenDTO;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -15,15 +17,54 @@ import java.util.UUID;
 @Repository
 public class TicketRepository {
 
+    private final RoomRepository roomRepository;
+    private Table<String, String, List<Object>> ticketTable;
+
+
+    public TicketRepository(RoomRepository roomRepository) {
+        this.roomRepository = roomRepository;
+        this.ticketTable = initTicketRepository(roomRepository);
+    }
+
     public Table<String, String, List<Object>> initTicketRepository(RoomRepository roomRepository) {
-        Table<String, String, List<Object>> ticketTable = HashBasedTable.create(roomRepository.getRoomRowsFromProps(),
+
+        Table<String, String, List<Object>> initTable = HashBasedTable.create(roomRepository.getRoomRowsFromProps(),
                 roomRepository.getRoomColumnsFromProps());
         for (Seat seat: roomRepository.getAvailableSeats()){
-            ticketTable.put(String.valueOf(seat.getRow()),
+            initTable.put(String.valueOf(seat.getRow()),
                     String.valueOf(seat.getColumn()),
                             new ArrayList<>(Arrays.asList(false, new Ticket(UUID.randomUUID(), new Seat(seat.getRow(), seat.getColumn())))));
         }
+        return initTable;
+    }
+
+    public boolean isSeatExist(Table<String, String, List<Object>> ticketTable, Seat seat){
+        return (ticketTable.containsRow(String.valueOf(seat.getRow())) && ticketTable.containsColumn(String.valueOf(seat.getColumn())));
+    }
+
+    public boolean isTicketForSeatPurchased(Table<String, String, List<Object>> ticketTable, Seat seat){
+        return (boolean)ticketTable.get(getSeatRowColumn(seat)[0], getSeatRowColumn(seat)[1]).get(0);
+    }
+
+
+    public Table<String, String, List<Object>> getTicketTable() {
         return ticketTable;
     }
 
+    public void setTicketTable(Table<String, String, List<Object>> ticketTable, Seat seat) {
+
+        this.ticketTable = ticketTable;
+    }
+
+    private String[] getSeatRowColumn(Seat seat){
+        return new String[]{String.valueOf(seat.getRow()), String.valueOf(seat.getColumn())};
+    }
+
+    public PurchasedTicketDTO postPurchaseTicket(Seat seat) {
+        return new PurchasedTicketDTO(new TokenDTO("token"), new TicketDTO(2, 3, 10));
+    }
+
+    public TicketDTO getRefundedTicketByToken(TokenDTO token) {
+        return new TicketDTO(5, 5, 50);
+    }
 }
