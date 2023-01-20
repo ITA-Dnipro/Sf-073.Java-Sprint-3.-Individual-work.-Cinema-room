496fedec2a99bd7a9f95f00f9e9db6447bb0b5bd
penzata <penzata86@yahoo.com>
2023-01-20 16:23:51 +0200

    minor fix

--- Cinema Room REST Service/task/src/cinema/rest/controller/CinemaController.java
+++ Cinema Room REST Service/task/src/cinema/rest/controller/CinemaController.java
@@ -10,7 +10,11 @@ import cinema.rest.dto.SeatDTO;
 import cinema.rest.dto.StatsDTO;
 import cinema.rest.dto.TicketDTO;
 import cinema.rest.dto.TokenDTO;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class CinemaController {
--- Cinema Room REST Service/task/src/cinema/rest/dto/CinemaRoomDTO.java
+++ Cinema Room REST Service/task/src/cinema/rest/dto/CinemaRoomDTO.java
@@ -2,13 +2,10 @@ package cinema.rest.dto;
 
 import cinema.domain.model.CinemaRoom;
 
-import javax.validation.constraints.Min;
 import java.util.List;
 
 public record CinemaRoomDTO(
-        @Min(1)
         int totalRows,
-        @Min(1)
         int totalColumns,
         List<SeatDTO> availableSeats) {
 
